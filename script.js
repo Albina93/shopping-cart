b677a44549639c8d43e21bce9114864b699085de
Albina93 <alba.tleubergen@gmail.com>
2026-03-26 19:02:59 -0400

    Removing products from the cart and total price updates

--- script.js
+++ script.js
@@ -23,19 +23,28 @@ function removeItem(event) {
 
 addProductBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  const name = productNameInput.value.trim();
-  const price = productPriceInput.value;
+  let name = productNameInput.value.trim();
+  let price = parseFloat(productPriceInput.value);
   // console.log(name, typeof price);
 
   // Checking for an empty product name, empty price, negative price
-  if (!name || isNaN(price) || price <= 0) {
+  if (!name || Number.isNaN(price) || price <= 0) {
     alert("Hey, you need to add valid product and price");
     return;
   }
   const li = document.createElement("li");
+  li.dataset.price = Number(price);
   li.textContent = `${name} - ${price}`;
+
+  const removeBtn = document.createElement("button");
+  removeBtn.textContent = "Remove";
+  removeBtn.style.marginLeft = "5px";
+
+  removeBtn.addEventListener("click", removeItem);
+
+  li.append(removeBtn);
   cart.append(li);
   productNameInput.value = "";
   productPriceInput.value = "";
-  updateTotalPrice(Number(price));
+  updateTotalPrice(price);
 });
